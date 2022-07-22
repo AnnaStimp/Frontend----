d792b476cef0559454597111587c9c4bef41875a
AnnaStimp <strashenkoanna@mail.ru>
2022-07-22 15:25:16 +0300

    create

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <ButtonMake />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<script>
+import ButtonMake from './components/ButtonMake.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ButtonMake
+  }
+}
+</script>
---
+++ src/components/ButtonMake.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="bg-sky-600 hover:bg-sky-700 ...">Заказать в Москву</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonMake',
+    mounted () {
+      console.log(this.$store.state.count)
+    }
+  }
+</script>
